2f87486cf83b30d992942b2fed2484ba01795872
samuelckl <59119742+samuelckl@users.noreply.github.com>
2024-09-21 18:50:32 +1000

    Fixed CoursesLisingView

--- GradeTracker/View/CoursesListingView.swift
+++ GradeTracker/View/CoursesListingView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 import SwiftData
 
 struct CoursesListingView: View {
-    @Query var courses: [Course]
+    
+    @State var showingSheet: Bool = false
+    @Binding var semester: Semester?
     
     var body: some View {
         
@@ -22,20 +24,30 @@ struct CoursesListingView: View {
                     
                 }
                 .padding(.bottom, 5)
-            }){
-                List {
-                    ForEach(semester?.courses ?? []) { course in
-                        NavigationLink(value: course) {
-                            RowView(item: course.title)
+            }) {
+                List (semester?.courses ?? []) { course in
+                    NavigationLink {
+                        NavigationStack{
+                            CourseView(course: course)
+                                .toolbar {
+                                    ToolbarItem(placement: .primaryAction) {
+                                        NavigationLink {
+                                            EditCourseView(course: course)
+                                        } label: {
+                                            Image(systemName: "square.and.pencil")
+                                        }
+                                    }
+                                }
+                                .navigationTitle(course.title)
                         }
+                    } label: {
+                        let totalMark = course.assignments.reduce(0) { $0 + ($1.mark ?? 0) }
+                        let totalWeight = course.assignments.reduce(0) { $0 + $1.weight }
+                        let totalWeight2 = totalWeight < 100 ? 100 : totalWeight
+
+                        RowView(item: course.title, rightView: ProgressDonutChart(current: totalMark, total: totalWeight2))
                     }
                 }
-            } label: {
-                let totalMark = course.assignments.reduce(0) { $0 + ($1.mark ?? 0) }
-                let totalWeight = course.assignments.reduce(0) { $0 + $1.weight }
-                let totalWeight2 = totalWeight < 100 ? 100 : totalWeight
-
-                RowView(item: course.title, rightView: ProgressDonutChart(current: totalMark, total: totalWeight2))
             }
             
         }
@@ -46,22 +58,20 @@ struct CoursesListingView: View {
         }
         
     }
-    
-    
 }
 
 
 
-    #Preview {
-        do {
-            let config = ModelConfiguration(isStoredInMemoryOnly: true)
-            let container = try ModelContainer(for: Course.self, configurations: config)
-            
-            return CoursesListingView(semester: .constant(nil))
-                .modelContainer(container)
-        } catch {
-            fatalError("Failed to create model container: \(error)")
-        }
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Course.self, Semester.self, configurations: config)
+        
+        return CoursesListingView(semester: .constant(Semester()))
+            .modelContainer(container)
+    } catch {
+        fatalError("Failed to create model container: \(error)")
     }
-    
+}
+
 
